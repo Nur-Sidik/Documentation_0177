a2b82da984b7d9fe69f4f1ee5cbd3e81e8c29f0a
Nur-Sidik <nursidik1500@gmail.com>
2025-07-06 22:10:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var index =
+[
+    [ "Introduction", "index.html#Introduction", null ],
+    [ "Operations", "index.html#Operations", null ],
+    [ "Pengunaan", "index.html#Cara", null ]
+];
---
+++ search/all_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['queue_0',['Documentation for Circular Queue',['../index.html',1,'']]],
+  ['queues_1',['Queues',['../class_queues.html',1,'Queues'],['../class_queues.html#a27d3ceab61177fd964098d2e279e45b6',1,'Queues::Queues()']]]
+];
